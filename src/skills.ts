c832a5a7853c0599f5849983e1b2c059585ce693
Sanerins <askosnitskiy@gmail.com>
2021-12-18 09:03:34 +0300

    Working implementation

--- src/skills.ts
+++ src/skills.ts
@@ -1,2 +1,54 @@
-const anExampleVariable = "Hello World"
-console.log(anExampleVariable)
+let amount : number = 0;
+
+function handleForm() {
+    if(amount <= 4) {
+        let name = (document.getElementById("skillName") as HTMLInputElement).value;
+        let percentage: number = +(document.getElementById("percentage") as HTMLInputElement).value;
+        createNewSkillConstruction(name, percentage);
+    }
+}
+
+function createNewSkillConstruction(name: string, percentage: number) {
+    amount += 1;
+    const place = document.getElementById("skillForm");
+    const parent = document.getElementsByClassName("card-plate__skills")[0];
+    let newDiv = document.createElement("div");
+    newDiv.className = "card-plate__skills__instance";
+    parent.insertBefore(newDiv, place);
+    let stat = document.createElement("div");
+    stat.className = "card-plate__skills__instance-stat";
+    let skillName = document.createElement("div");
+    skillName.className = "card-plate__skills__instance-name";
+    newDiv.appendChild(stat);
+    newDiv.insertBefore(skillName, stat);
+    let content = document.createTextNode(name);
+    skillName.appendChild(content);
+    let deleteButton = document.createElement("input");
+    deleteButton.type = "image";
+    deleteButton.src = "resources/trashBin.png";
+    deleteButton.className = "card-plate__skills__instance-stat-delete";
+    stat.appendChild(deleteButton);
+    deleteButton.addEventListener('click', function(){
+        deleteSkillConstruction(this)
+    });
+    let percent = document.createElement("div");
+    percent.className = "card-plate__skills__instance-stat-percentage";
+    stat.insertBefore(percent, deleteButton);
+    content = document.createTextNode(percentage.toString());
+    percent.appendChild(content);
+    let bar = document.createElement("div");
+    bar.className = "card-plate__skills__instance-stat-bar";
+    stat.insertBefore(bar, percent);
+    let barFilled = document.createElement("div");
+    barFilled.className = "card-plate__skills__instance-stat-bar-filled";
+    barFilled.style.width = percentage + "%";
+    bar.appendChild(barFilled);
+}
+
+function deleteSkillConstruction(element: HTMLInputElement) {
+    amount -= 1;
+    let skill = element.parentElement.parentElement;
+    skill.innerHTML = "";
+    skill.remove();
+    return this.id;
+}
